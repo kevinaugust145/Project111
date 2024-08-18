0e5d84455b086e702925fc338aae010d45f1c7e7
KevinHung <kevinaugust145@gmail.com>
2024-08-18 18:05:33 +0800

    Initial Commit

---
+++ Project111Tests/Project111Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project111Tests.swift
+//  Project111Tests
+//
+//  Created by 洪宗燦 on 2024/8/18.
+//
+
+import Testing
+@testable import Project111
+
+struct Project111Tests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
